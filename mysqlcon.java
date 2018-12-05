c5ff33bea9cdd0d2f2017833bcc52e58afab7837
skb283smart <skb283smart@gmail.com>
2018-12-06 01:14:23 +0530

    java file

---
+++ mysqlcon.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import java.util.*;
+
+class MysqlCon{
+	public static void main(String args[]){
+	try{
+		Class.forName("com.Mysql.JdbcDriver");
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/wsr","root","");
+		Statement smt= con.createStatement();
+		ResultSet rs = smt.executeQuery("Select * from t_wsr_accesslog");
+		while(rs.next())
+			System.out.println(rs.getInt(1)+rs.getString(2));
+		con.close();
+	}		
+	catch(Exception e){
+		System.out.println(e);
+	}
+}
+}
